72e17ed3515ee8d6b6977832094ac69b75aa4065
SadmanShahriar75 <130807436+SadmanShahriar75@users.noreply.github.com>
2023-04-15 06:47:14 +0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Software+sdk\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Sadman\\Desktop\\flutter\\chat_bot" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Software+sdk\\flutter"
+  "PROJECT_DIR=C:\\Users\\Sadman\\Desktop\\flutter\\chat_bot"
+  "FLUTTER_ROOT=C:\\Software+sdk\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Sadman\\Desktop\\flutter\\chat_bot\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Sadman\\Desktop\\flutter\\chat_bot"
+  "FLUTTER_TARGET=C:\\Users\\Sadman\\Desktop\\flutter\\chat_bot\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Sadman\\Desktop\\flutter\\chat_bot\\.dart_tool\\package_config.json"
+)
